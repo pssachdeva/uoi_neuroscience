e3ff3153aa4b1d5807210ed137643ff729b26d3d
Pratik Sachdeva <pratik.sachdeva23@gmail.com>
2018-12-27 10:42:11 -0800

    docstring

--- coupling/pvc11_coupling.py
+++ coupling/pvc11_coupling.py
@@ -1,3 +1,11 @@
+"""
+Performs a cross-validated coupling fit on spiking data from the PVC11 CRCNS
+database. This dataset consists of primary visual cortex spiking responses to
+gratings.
+
+This script performs and stores coupling models on this dataset using a
+desired fitting procedure.
+"""
 import argparse
 import h5py
 
@@ -23,12 +31,19 @@ def main(args):
         model='c',
         method=args.method,
         class_labels=class_labels,
-        targets=0,
+        targets=None,
         n_folds=args.n_folds,
         random_state=args.random_state,
         metrics=['r2', 'BIC', 'AIC'],
+
         # general options
+        fit_intercept=True,
         normalize=args.normalize,
+
+        # Lasso specific
+        cv=args.cv,
+        max_iter=args.max_iter,
+
         # UoI Lasso specific
         estimation_score=args.estimation_score
     )
@@ -37,6 +52,7 @@ def main(args):
     group = results_file.create_group(args.results_group)
     # place results in group
     for key in results.keys():
+        # need to handle training and test folds separately
         if key == 'training_folds' or key == 'test_folds':
             folds_group = group.create_group(key)
             for fold_key, fold_val in results[key].items():
@@ -54,12 +70,15 @@ if __name__ == '__main__':
     parser.add_argument('--results_path')
     parser.add_argument('--results_group')
     parser.add_argument('--method')
-    parser.add_argument('--n_folds', type=int)
-    # optional arguments
-    parser.add_argument('--transform', default='square_root')
-    parser.add_argument('--normalize', action='store_true')
-    parser.add_argument('--estimation_score', default='r2')
+    parser.add_argument('--n_folds', type=int, default=10)
     parser.add_argument('--random_state')
+    parser.add_argument('--transform', default='square_root')
+    # fitter object arguments
+    parser.add_argument('--normalize', action='store_true')
+    parser.add_argument('--cv', type=int, default=10)
+    parser.add_argument('--max_iter', type=int, default=5000)
+    # UoI Lasso arguments
+    parser.add_argument('--estimation_score', default='r2')
 
     args = parser.parse_args()
 
