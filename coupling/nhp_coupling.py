b3550ef14e058add62f962872a4e89e1a3bb84c6
Pratik Sachdeva <pratik.sachdeva23@gmail.com>
2019-06-05 16:14:14 -0700

    nhp coupling

--- coupling/nhp_coupling.py
+++ coupling/nhp_coupling.py
@@ -30,38 +30,47 @@ def main(args):
         region=args.region,
         transform=args.transform
     )
+    # clear out empty units
     Y = Y[:, np.argwhere(Y.sum(axis=0) > 0).ravel()]
 
     # create solver object
     solver = SEMSolver(Y=Y)
 
+    # create args
+    args = {
+        'model': 'c',
+        'method': args.method,
+        'targets': [0],
+        'n_folds': args.n_folds,
+        'random_state': random_state,
+        'verbose': args.verbose,
+        'fit_intercept': True,
+        'max_iter': args.max_iter,
+        'metrics': ['r2', 'AIC', 'BIC']
+    }
+
+    if args.method == 'Lasso':
+        args['normalize'] = args.normalize
+        args['cv'] = args.cv
+    elif 'UoI' in args.method:
+        # important: we use normalize/standardize to mean the same thing
+        args['standardize'] = args.normalize
+        args['n_boots_sel'] = args.n_boots_sel
+        args['n_boots_est'] = args.n_boots_est
+        args['selection_frac'] = args.selection_frac
+        args['estimation_frac'] = args.estimation_frac
+        args['n_lambdas'] = args.n_lambdas
+        args['stability_selection'] = args.stability_selection
+        args['estimation_score'] = args.estimation_score
+    else:
+        raise ValueError('Method is not valid.')
+
+    if args.method == 'UoI_Poisson':
+        args['solver'] = args.solver
+        args['metrics'] = ['AIC', 'BIC']
+
     # perform cross-validated coupling fits
-    results = solver.estimation(
-        model='c',
-        method=args.method,
-        targets=None,
-        n_folds=args.n_folds,
-        random_state=random_state,
-        metrics=['r2', 'BIC', 'AIC'],
-        verbose=args.verbose,
-
-        # general options
-        fit_intercept=True,
-        normalize=args.normalize,
-
-        # Lasso specific
-        cv=args.cv,
-        max_iter=args.max_iter,
-
-        # UoI Lasso specific
-        n_boots_sel=args.n_boots_sel,
-        n_boots_est=args.n_boots_est,
-        selection_frac=args.selection_frac,
-        estimation_frac=args.estimation_frac,
-        n_lambdas=args.n_lambdas,
-        stability_selection=args.stability_selection,
-        estimation_score=args.estimation_score
-    )
+    results = solver.estimation(**args)
 
     results_file = h5py.File(args.results_path, 'a')
     group = results_file.create_group(args.results_group)
@@ -81,28 +90,39 @@ def main(args):
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
 
+    # required arguments
     parser.add_argument('--data_path')
     parser.add_argument('--results_path')
     parser.add_argument('--results_group')
     parser.add_argument('--method')
+
+    # NHP arguments
     parser.add_argument('--region', default='M1')
     parser.add_argument('--bin_width', type=float, default=0.5)
     parser.add_argument('--n_folds', type=int, default=10)
-    parser.add_argument('--random_state', type=int, default=-1)
     parser.add_argument('--transform', default='square_root')
-    parser.add_argument('--verbose', action='store_true')
+
     # fitter object arguments
     parser.add_argument('--normalize', action='store_true')
+    parser.add_argument('--random_state', type=int, default=-1)
+
+    # LassoCV
     parser.add_argument('--cv', type=int, default=10)
     parser.add_argument('--max_iter', type=int, default=5000)
-    # UoI Lasso arguments
+
+    # UoI arguments
     parser.add_argument('--n_boots_sel', type=int, default=30)
     parser.add_argument('--n_boots_est', type=int, default=30)
     parser.add_argument('--selection_frac', type=float, default=0.8)
     parser.add_argument('--estimation_frac', type=float, default=0.8)
-    parser.add_argument('--n_lambdas', type=int, default=48)
+    parser.add_argument('--n_lambdas', type=int, default=50)
     parser.add_argument('--stability_selection', type=float, default=1.)
     parser.add_argument('--estimation_score', default='r2')
+
+    # UoI Poisson arguments
+    parser.add_argument('--solver', default='lbfgs')
+
+    parser.add_argument('--verbose', action='store_true')
 
     args = parser.parse_args()
 
